1d575dfcc688e79fc3289f2738f6ca0e9df28e6f
Ramesh Aryal <ramesh.aryal@socoro.com.au>
2024-03-19 13:16:44 +1100

    pulled the data and use the streamline

--- app/dashboard/customers/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function page() {
-  return <div>Customer page</div>;
-}
-
-export default page;
--- app/dashboard/invoices/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function page() {
-  return <div>Invoices page</div>;
-}
-
-export default page;
